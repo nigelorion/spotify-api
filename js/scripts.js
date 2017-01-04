16d4aa2740ee45e7e2dc749796fc6061e46f47e9
Nigel Burnett and Rafael Perez <nigel@>
2017-01-03 16:12:11 -0800

    Add alarm logic and click functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,56 @@
-function Clock(alarmTime) {
-  this.time = "";
+function Clock() {
+  this.time = 0;
 }
-function AlarmClock(){
+function AlarmTime(){
   this.alarmTime = 0;
+  this.hours = 0;
+  this.minutes = 0;
+  this.seconds = 0;
 }
-
 
 Clock.prototype.getTime = function () {
- this.time = moment().format('H:mm:ss');
+ this.time = moment().format('h:mm:ss');
 };
 
-AlarmClock.prototype.setAlarm = function (alarmTime) {
- this.alarmTime = alarmTime;
-};
-AlarmClock.prototype.checkAlarm = function () {
+AlarmTime.prototype.setAlarm = function (hours, minutes, seconds) {
+ this.alarmTime = moment().hours(hours).minutes(minutes).seconds(seconds).format('h:mm:ss');
 
 };
-
-AlarmClock.prototype.alarm = function () {
-
+AlarmTime.prototype.checkAlarm = function (clocktime) {
+  if (this.alarmTime === clocktime){
+    this.alarm();
+  }
 };
+
+AlarmTime.prototype.alarm = function () {
+  $("p").css("animation", "rotate .6s ease-in-out infinite");
+};
+
+AlarmTime.prototype.alarmClicks = function (thisID) {
+
+  if (thisID === "up__hours") {
+    this.hours ++;
+  }
+  else if (thisID === "up__minutes") {
+    this.minutes ++;
+  }
+  else if (thisID === "up__seconds") {
+    this.seconds ++;
+  }
+  else if (thisID === "down__hours") {
+    this.hours --;
+  }
+  else if (thisID === "down__minutes") {
+    this.minutes --;
+  }
+  else if (thisID === "down__seconds") {
+    this.seconds --;
+  }
+
+  this.setAlarm(this.hours, this.minutes, this.seconds);
+
+}
 
 
 exports.clockModule = Clock;
-exports.clockAlarmModule = AlarmClock;
+exports.alarmModule = AlarmTime;
--- js/time-interface.js
+++ js/time-interface.js
@@ -1,11 +1,27 @@
 var Clock = require('./../js/scripts.js').clockModule;
-var AlarmClock = require('./../js/scripts.js').AlarmclockModule;
+var AlarmClock = require('./../js/scripts.js').alarmModule;
 
 $(document).ready(function(){
-  var clock = new Clock("asdf");
+  var clock = new Clock();
+  var alarm = new AlarmClock();
 
  setInterval (function() {
    clock.getTime();
    $('#time').text(clock.time);
+   alarm.checkAlarm(clock.time);
  }, 1000);
-});
+
+$('#alarm').text("12:00:00");
+
+ $("p").click(function() {
+   var myID = $(this).attr("id");
+  $("#alarm").text(alarm.alarmClicks(myID));
+  $('#alarm').text(alarm.alarmTime);
+ });
+
+ $(".alarm__clock__off").click(function() {
+   $("p").css("animation", "rotate .1s ease-in-out");
+   console.log("hit");
+ });
+
+ });
